0604b50e64089e766b82e4d5d56cf390034e3af5
Sidney Tan <sidneyjt@uci.edu>
2020-11-26 16:40:44 -0800

    Program now takes arguments on launch.

--- src/main/java/stan/draftbot/Main.java
+++ src/main/java/stan/draftbot/Main.java
@@ -5,7 +5,11 @@ public class Main
     public static void main(String[] args)
     {
         try {
-            DraftBot bot = new DraftBot();
+            if (args.length < 1) {
+                throw new Exception("Must have Token as first argument.");
+            } else {
+                DraftBot bot = new DraftBot(args);
+            }
         }catch (Exception e)
         {
             e.printStackTrace();
